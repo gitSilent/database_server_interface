0b028bcadf67487c9b7878f1dc06442950a20fcb
gitSilent <97840080+gitSilent@users.noreply.github.com>
2022-11-01 13:23:49 +0300

    begin_post

--- index.js
+++ index.js
@@ -9,10 +9,23 @@ const pool = createPool({
     user : 'root',
     password : '1234',
     database : 'library1234',
+    bigIntAsNumber : true,
+    insertIdAsNumber : true
 });
 
 
-app.get("/", async(req, res) => {
+app.get("/allBooks", async(req, res) => {
+    let connection = await pool.getConnection();
+    const response = await connection.query('SELECT * from books', (err, data) => {
+        console.log(data)
+    })
+    // console.log(response)
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
+    res.send(response)
+    
+})
+app.get("/allAuthors", async(req, res) => {
     let connection = await pool.getConnection();
     const response = await connection.query('SELECT * from authors', (err, data) => {
         console.log(data)
@@ -25,13 +38,23 @@ app.get("/", async(req, res) => {
 })
 
 
-app.post("/", (req, res) => {
-    pool.query("insert into authors values(0, \"Ф.М.Достоевский\", \"1821-11-11\")", function(err, results) {
-        pool.release()
-    if(err) console.log(err);
-    console.log(results);
-    return(results)
-    });
+app.post("/postAuthor", async(req, res) => {
+    let connection = await pool.getConnection();
+    let author_info = req.body;
+    
+    const post = await connection.query(`insert into authors values (${author_info.id_author}, ${JSON.stringify(author_info.name_author)}, ${JSON.stringify(author_info.birthday_author)})`, (err, data) => {
+        console.log(data)
+    })
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
+    res.json(post)
+
+    // pool.query("insert into authors values(0, \"Ф.М.Достоевский\", \"1821-11-11\")", function(err, results) {
+    //     pool.release()
+    // if(err) console.log(err);
+    // console.log(results);
+    // return(results)
+    // });
 })
 
 // mysql.format()
--- interface/main.js
+++ interface/main.js
@@ -5,3 +5,71 @@ fetch("http://localhost:4000/")
 .then((val)=>{
     console.log(val)
 })
+
+const btnAddAuthor = document.querySelector('.btn_add_author');
+
+function fillTableBooks(id){
+    
+    fetch(`http://localhost:4000/${id}`)
+    .then(val =>
+        val.json()
+    )
+    .then((val)=>{
+        const table = document.createElement('table');
+        table.className = 'table_books';
+
+        let table_header = document.createElement('thead');
+        let tr = document.createElement('tr');
+
+        let table_body= document.createElement('tbody');
+        
+        for(let key in val[0]){
+            const th = document.createElement('th');
+            th.textContent = `${key}`
+            tr.append(th)
+        }
+        
+        table_header.append(tr);
+        table.append(table_header)
+        
+        val.forEach((el,index) => {
+            let tr = document.createElement('tr');
+            for(let key in el) {
+                const th = document.createElement('th');
+                th.textContent = el[key]
+                tr.append(th)
+            };
+            table_body.append(tr)
+        });
+
+        table.append(table_body)
+        
+        
+        document.body.append(table);
+
+        console.log(val)
+    })
+}
+
+function addAuthor(name_author, birthday_author){
+    fetch(`http://localhost:4000/postAuthor`,{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body:{
+            id_author : 0,
+            name_author,
+            birthday_author
+        }
+    })
+    
+}
+
+btnAddAuthor.addEventListener('click', ()=>{
+    addAuthor('Блок','1900-01-01')
+})
+
+
+fillTableBooks("allBooks")
+fillTableBooks("allAuthors")
