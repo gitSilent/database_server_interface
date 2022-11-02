2728e43eeda95fdb2f7bc8d3bf2dc326f6aad69f
gitSilent <97840080+gitSilent@users.noreply.github.com>
2022-11-02 11:27:51 +0300

    post_to_refact

--- index.js
+++ index.js
@@ -1,86 +1,142 @@
 import express, { json } from "express";
-import cors from 'cors'
-import { createConnection, createPool } from 'mariadb';
+import cors from "cors";
+import { createConnection, createPool } from "mariadb";
 import bodyParser from "body-parser";
+
+function users(req, res) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+}
 
 const app = express();
 
-app.use(express.json())
-// app.use(cors())
-app.use(express.urlencoded({extended:true}))
-
+app.use(
+  express.json({
+    type: ["application/json", "text/plain"],
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 
 const pool = createPool({
-    host : '127.0.0.1',
-    user : 'root',
-    password : '1234',
-    database : 'library',
-    bigIntAsNumber : true,
-    insertIdAsNumber : true
+  host: "127.0.0.1",
+  user: "root",
+  password: "1234",
+  database: "library1234",
+  bigIntAsNumber: true,
+  insertIdAsNumber: true,
 });
 
+app.get("/get/allBooks", async (req, res) => {
+  let connection = await pool.getConnection();
+  const response = await connection.query(
+    "SELECT * from books",
+    (err, data) => {
+      console.log(data);
+    }
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
+  res.send(response);
+});
 
-app.get("/get/allBooks", async(req, res) => {
+app.get("/get/allAuthors", async (req, res) => {
+  let connection = await pool.getConnection();
+  const response = await connection.query(
+    "SELECT * from authors",
+    (err, data) => {
+      console.log(data);
+    }
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.send(response);
+});
+
+app.get("/:table", async (req, res) => {
     let connection = await pool.getConnection();
-    const response = await connection.query('SELECT * from books', (err, data) => {
-        console.log(data)
-    })
-    // console.log(response)
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
-    res.send(response)
-    
+    res.send(await connection.query(
+    "SELECT * from authors",
+    (err, data) => {
+      console.log(data);
+    }
+  ))
 })
-app.get("/get/allAuthors", async(req, res) => {
-    let connection = await pool.getConnection();
-    const response = await connection.query('SELECT * from authors', (err, data) => {
-        console.log(data)
-    })
-    // console.log(response)
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.send(response)
-    
-})
 
+app.post("/post/author", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  let connection = await pool.getConnection();
+  let author_info = req.body;
 
-app.post("/post/author", async(req, res) => {
-    let connection = await pool.getConnection();
-    let author_info = req.body;
-    console.log('ny')
+  const post = await connection.query(
+    `insert into authors(name_author, birthday_author) values ( ${JSON.stringify(
+      author_info.name_author
+    )}, ${JSON.stringify(author_info.birthday_author)})`,
+    (err, data) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      if (err) throw err;
+      console.log(data);
+    }
+  );
+
+  res.json(post);
+
+});
+
+app.post("/post/book", async (req, res) => {
     console.log(req.body)
-    
-    const post = await connection.query(`insert into authors values (${author_info.id_author}, ${JSON.stringify(author_info.name_author)}, ${JSON.stringify(author_info.birthday_author)})`, (err, data) => {
-       if(err) throw (err)
-        console.log(data)
-    })
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  let connection = await pool.getConnection();
+  let book_info = req.body;
 
-    res.json(post)
+  const post = await connection.query(
+    `insert into books (title_book,id_ganre,id_author,qty_books)
+    values (${JSON.stringify(book_info.title_book)},
+        ${book_info.id_ganre},
+        ${book_info.id_author},
+        ${book_info.qty_books}
+    )`, 
+    (err, data) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      if (err) throw err;
+      console.log(data);
+    }
+  );
 
-    // pool.query("insert into authors values(0, \"Ф.М.Достоевский\", \"1821-11-11\")", function(err, results) {
-    //     pool.release()
-    // if(err) console.log(err);
-    // console.log(results);
-    // return(results)
-    // });
-})
+  res.json(post);
+});
 
-app.put("/put/moving", async(req,res)=>{
-    let connection = await pool.getConnection();
-    let moving_info = req.body;
+app.put("/put/moving", async (req, res) => {
+  let connection = await pool.getConnection();
+  let moving_info = req.body;
 
-    const put = await connection.query(`update moving set date_in=${JSON.stringify(moving_info.date_in)} where id_note = ${moving_info.id_note}`, (err,data)=>{
-        console.log(data)
-    })
-    res.json(put)
-
-})
+  const put = await connection.query(
+    `update moving set date_in=${JSON.stringify(
+      moving_info.date_in
+    )} where id_note = ${moving_info.id_note}`,
+    (err, data) => {
+      console.log(data);
+    }
+  );
+  res.json(put);
+});
 
 // mysql.format()
 
 app.listen(4000, () => {
-console.log('Server is running at port 4000');
+  console.log("Server is running at port 4000");
 });
--- interface/main.js
+++ interface/main.js
@@ -1,89 +1,92 @@
-// fetch("http://localhost:4000/")
-// .then(val =>
-//     val.json()
-// )
-// .then((val)=>{
-//     console.log(val)
-// })
+const btnAddAuthor = document.querySelector(".btn_add_author");
+const inputNameAuthor = document.querySelector(".input_nameAuthor");
+const inputBirthdayAuthor = document.querySelector(".input_birthdayAuthor");
 
-const btnAddAuthor = document.querySelector('.btn_add_author');
-const inputNameAuthor = document.querySelector('.input_nameAuthor')
-const inputBirthdayAuthor = document.querySelector('.input_birthdayAuthor')
+const btnAddBook = document.querySelector(".btn_add_book");
+const inputTitleBook = document.querySelector(".input_titleBook");
+const inputIdGanre = document.querySelector(".input_idGanre");
+const inputIdAuthor = document.querySelector(".input_idAuthor");
+const inputQtyBooks = document.querySelector(".input_qtyBooks");
 
-function fillTableBooks(id){
-    
-    fetch(`http://127.0.0.1:4000/get/${id}`)
-    .then(val =>
-        val.json()
-    )
-    .then((val)=>{
-        const table = document.createElement('table');
-        table.className = `table_${id}`;
+function fillTable(id) {
+  fetch(`http://127.0.0.1:4000/get/${id}`)
+    .then((val) => val.json())
+    .then((val) => {
+      const table = document.createElement("table");
+      table.className = `table_${id}`;
 
-        let table_header = document.createElement('thead');
-        let tr = document.createElement('tr');
+      let table_header = document.createElement("thead");
+      let tr = document.createElement("tr");
 
-        let table_body= document.createElement('tbody');
-        
-        for(let key in val[0]){
-            const th = document.createElement('th');
-            th.textContent = `${key}`
-            tr.append(th)
+      let table_body = document.createElement("tbody");
+
+      for (let key in val[0]) {
+        const th = document.createElement("th");
+        th.textContent = `${key}`;
+        tr.append(th);
+      }
+
+      table_header.append(tr);
+      table.append(table_header);
+
+      val.forEach((el, index) => {
+        let tr = document.createElement("tr");
+        for (let key in el) {
+          const th = document.createElement("th");
+          th.textContent = el[key];
+          tr.append(th);
         }
-        
-        table_header.append(tr);
-        table.append(table_header)
-        
-        val.forEach((el,index) => {
-            let tr = document.createElement('tr');
-            for(let key in el) {
-                const th = document.createElement('th');
-                th.textContent = el[key]
-                tr.append(th)
-            };
-            table_body.append(tr)
-        });
+        table_body.append(tr);
+      });
 
-        table.append(table_body)
-        
-        
-        document.body.append(table);
+      table.append(table_body);
 
-        console.log(val)
-    })
+      document.body.append(table);
+
+      console.log(val);
+    });
 }
 
-function addAuthor(name_author, birthday_author){
-    console.log()
-    fetch(`http://127.0.0.1:4000/post/author`,{
-        method: 'POST',
-        headers:{
-            'accept': 'application/json',
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            id_author : 0,
-            name_author: name_author,
-            birthday_author: birthday_author
-        }),
-        // body: "stroka",
-        mode: 'no-cors'
-    })
-    
+function addAuthor(name_author, birthday_author) {
+  fetch(`http://127.0.0.1:4000/post/author`, {
+    method: "POST",
+    body: JSON.stringify({
+      name_author: name_author,
+      birthday_author: birthday_author,
+    }),
+  })
+    .then((data) => data)
+      .then((data) => {
+          console.log(data)
+          fillTable("allAuthors")
+      });
 }
 
+function addBook(title_book, id_ganre, id_author, qty_books) {
+   
+  fetch(`http://127.0.0.1:4000/post/book`, {
+    method: "POST",
+    body: JSON.stringify({
+        title_book,
+        id_ganre,
+        id_author,
+        qty_books
+    }),
+  })
+    .then((data) => data)
+      .then((data) => {
+        fillTable("allBooks")
+    });
+}
 
-btnAddAuthor.addEventListener('click', ()=>{
-    delete document.querySelector('.table_allBooks')
-    // addAuthor('Блок','1900-01-01')
-    console.log(inputNameAuthor.value)
-    console.log(inputBirthdayAuthor.value)
-    addAuthor(inputNameAuthor.value,inputBirthdayAuthor.value)
-    
-    // fillTableBooks("allAuthors")
-    
-})
+btnAddAuthor.addEventListener("click", () => {
+    document.querySelector(".table_allAuthors").remove();
+    addAuthor(inputNameAuthor.value, inputBirthdayAuthor.value)    
+});
 
+btnAddBook.addEventListener("click", () => {
+  document.querySelector(".table_allBooks").remove();
+  addBook(inputTitleBook.value, inputIdGanre.value, inputIdAuthor.value, inputQtyBooks.value);
+});
 
-// fillTableBooks("allBooks")
-fillTableBooks("allAuthors")
+fillTable("allBooks");
